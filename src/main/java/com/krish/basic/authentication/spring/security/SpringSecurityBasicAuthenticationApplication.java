28ca94d629a9ad68dacb8e997d0e4da1eb6cdca6
Krishna <surekrrishna@gmail.com>
2018-10-29 12:39:17 +0530

    spring rest basic authentication using spring security and running tomcat port 8089 by simply adding server.port = 8089 automatically picks the port

---
+++ src/main/java/com/krish/basic/authentication/spring/security/SpringSecurityBasicAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.krish.basic.authentication.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBasicAuthenticationApplication.class, args);
+	}
+}
